909d01dd827352bf62073c82e54bfce3021d869b
Diggy Mint <Diggy Mint>
2016-12-26 21:00:31 -0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by ex dee on 12/26/2016.
+ */
+
+$(function () {
+    var $window = $(window);
+
+    $('section[data-type="hero-background"]').each(function () {
+        var $bjobj = $(this);
+        $window.scroll(function () {
+
+            var yPos = (-($window.scrollTop() / $bjobj.data('speed'))) + 75;
+
+            var coords = '25% ' + yPos + 'px';
+
+            $bjobj.css({backgroundPosition: coords});
+            
+        });
+    });
+});
+
